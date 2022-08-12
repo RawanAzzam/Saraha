19ecb9c612b7f9e0f86da029db10959a0dc23558
Rawan <rawanazzam153@gmail.com>
2022-08-12 18:02:51 +0300

    rawan

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'contact',
+    component:ContactComponent
+    },
+    {
+      path:'',
+      component:HomeComponent
+      },
+ {
+        path:'about',
+        component:AboutComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
