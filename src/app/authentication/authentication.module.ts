58ff09e68d2b0344098b769d26cb3c8648b35839
amalmomani99@yahoo.com <amalmomani99@yahoo.com>
2022-08-16 14:56:21 +0300

    Login

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import {  MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -14,6 +16,8 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     CommonModule,
     AuthenticationRoutingModule,
+    ReactiveFormsModule,
+    MatFormFieldModule
   ]
 })
 export class AuthenticationModule { }
