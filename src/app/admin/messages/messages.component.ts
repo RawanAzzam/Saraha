0080a4c4d632b75dc7dc0f022d6859899e95a2f7
amalmomani99@yahoo.com <amalmomani99@yahoo.com>
2022-08-22 17:47:41 +0300

    GetContact

--- src/app/admin/messages/messages.component.ts
+++ src/app/admin/messages/messages.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ContactUsService } from 'src/app/Services/contact-us.service';
 
 @Component({
   selector: 'app-messages',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
-
-  constructor() { }
-
+  constructor( public contactUs:ContactUsService){}
   ngOnInit(): void {
+
+    this.contactUs.GetAll();
+
   }
+  @Input()contactusid:number| undefined;
+  @Input()username:string| undefined;
+  @Input()email:string| undefined;
+  @Input()message:string| undefined;
+
 
 }
+
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+// }
