9c40dc253a0fb9a32b68d399c6ef974574267a62
Waed Sharea <waedshare98@gmail.com>
2022-08-23 10:44:14 +0300

    waed

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,4 +1,4 @@
-saimport { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AsiderbarComponent } from './asiderbar/asiderbar.component';
