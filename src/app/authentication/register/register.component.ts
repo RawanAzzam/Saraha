ad0673066dda067f0827538c86d5ca9ca4585a25
Waed Sharea <waedshare98@gmail.com>
2022-08-23 14:04:28 +0300

    waed

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl,FormGroup, Validators } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -6,6 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  email : FormControl = new FormControl('',[Validators.required,Validators.email]);
+  password : FormControl = new FormControl('',[Validators.required,Validators.minLength(8)]);
+  phonenumber : FormControl = new FormControl('',[Validators.required,Validators.minLength(10),Validators.pattern("^[0-9]{10,12}$")]);
+
+  
+
+
+
 
   constructor() { }
 
