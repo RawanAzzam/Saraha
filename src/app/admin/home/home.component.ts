02ab537a92ec7c5410ccfbed666c2b2bc0100fe6
Waed Sharea <waedshare98@gmail.com>
2022-08-23 18:27:19 +0300

    waed

--- src/app/admin/home/home.component.ts
+++ src/app/admin/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/Services/user.service';
+
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor( private user:UserService) { }
 
   ngOnInit(): void {
   }
--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl,FormGroup, Validators } from '@angular/forms';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxSpinnerService } from 'ngx-spinner';
+import {ToastrService} from 'ngx-toastr';
 
 
 
@@ -10,16 +12,17 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  email : FormControl = new FormControl('',[Validators.required,Validators.email]);
-  password : FormControl = new FormControl('',[Validators.required,Validators.minLength(8)]);
-  phonenumber : FormControl = new FormControl('',[Validators.required,Validators.minLength(10),Validators.pattern("^[0-9]{10,12}$")]);
+  registerForm :FormGroup =  new FormGroup({  email :  new FormControl('',[Validators.required,Validators.email]),
+  password : new FormControl('',[Validators.required,Validators.minLength(8)]),
+  phonenumber : new FormControl('',[Validators.required,Validators.minLength(10),Validators.pattern("^[0-9]{10,12}$")]),
 
-  
-
-
+})
 
 
-  constructor() { }
+
+
+
+  constructor(private toaster:ToastrService, private spinner :NgxSpinnerService) { }
 
   ngOnInit(): void {
   }
