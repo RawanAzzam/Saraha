d65c44694273ec96f96ec48b4b7a42e7efd16b58
DELL <DELL@DESKTOP-Q4615I5>
2022-08-31 13:15:57 +0300

    saja tawalbeh

--- src/app/admin/home/home.component.ts
+++ src/app/admin/home/home.component.ts
@@ -7,7 +7,7 @@ import { PurchaseService } from 'src/app/Services/purchase.service';
 import { ReportService } from 'src/app/Services/report.service';
 import { UserService } from 'src/app/Services/user.service';
 import DatalabelsPlugin from 'chartjs-plugin-datalabels';
-import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { Chart, ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 import { SarahaWeatherService } from 'src/app/Services/saraha-weather.service';
 import { LoginService } from 'src/app/Services/login.service';
@@ -31,6 +31,8 @@ export class HomeComponent implements OnInit {
 user :any =  this.userservice.getUserById(Number(localStorage.getItem('userId')));
 
 ngOnInit(): void {
+  this.featureService.FeatureName();
+  this.featureService.FeatureTotalSales();
      this.userservice.getAllLoginUsers();
 
    this.contactUs.GetAll();
@@ -48,8 +50,7 @@ console.log(this.weatherService.weather);
    this.message.getAllMessages();
    this.userservice.getActivePepole();
    this.order.GetOrders();
-   this.featureService.FeatureName();
-   this.featureService.FeatureTotalSales();
+  
 
   }
 // ********************************
@@ -58,51 +59,118 @@ console.log(this.weatherService.weather);
 public hi()
 {
   this.weatherService.GetWeather();
+}
+BarChart :any= [];
 
+canvas: any;
+ctx: any;
 
+@ViewChild('mychart') mychart:any;
+ngAfterViewInit(){
+  this.canvas = this.mychart.nativeElement; 
+  this.ctx = this.canvas.getContext('2d');
+  new Chart(this.ctx, { 
+
+    type: 'bar',
+
+    data: {
+
+      labels: this.featureService.servName,
+
+      datasets: [{        
+
+        data: this.featureService.servSales,
+
+        backgroundColor: [
+
+          'rgba(255, 99, 132, 0.2)',
+
+          'rgba(255, 159, 64, 0.2)',
+
+          'rgba(255, 205, 86, 0.2)',
+
+          'rgba(75, 192, 192, 0.2)',
+
+          'rgba(54, 162, 235, 0.2)',
+
+          'rgba(153, 102, 255, 0.2)',
+
+          'rgba(201, 203, 207, 0.2)'
+
+        ],
+
+        borderColor: [
+
+          'rgb(255, 99, 132)',
+
+          'rgb(255, 159, 64)',
+
+          'rgb(255, 205, 86)',
+
+          'rgb(75, 192, 192)',
+
+          'rgb(54, 162, 235)',
+
+          'rgb(153, 102, 255)',
+
+          'rgb(201, 203, 207)'
+
+        ],
+
+        borderWidth: 1
+
+      }]
+
+    }
+
+});
 
 }
-  // Pie
-  public pieChartOptions: ChartConfiguration['options'] = {
-    responsive: true,
-    plugins: {
-      legend: {
-        display: true,
-        position: 'top',
-      },
-      datalabels: {
-        formatter: (value, ctx) => {
-          if (ctx.chart.data.labels) {
-            return ctx.chart.data.labels[ctx.dataIndex];
-          }
-        },
-      },
-    }
-  };
-  public pieChartData: ChartData<'pie', number[], string | string[]> = {
-    labels: this.featureService.servName,
-    datasets:  [ {
-      data: this.featureService.servSales,
-    } ]
-  };
-  
-  // pieChartLabels = this.featureService.servName;
-//   pieChartData:any = [
-//     { 
-//         data: this.featureService.servSales
+}
+//   // Pie
+//   public pieChartOptions: ChartConfiguration['options'] = {
+//     responsive: true,
+//     plugins: {
+//       legend: {
+//         display: true,
+//         position: 'top',
+//       },
+//       datalabels: {
+//         formatter: (value, ctx) => {
+//           if (ctx.chart.data.labels) {
+//             return ctx.chart.data.labels[ctx.dataIndex];
+//           }
+//         },
+//       },
 //     }
-// ];
-  public pieChartType: ChartType = 'pie';
-  public pieChartPlugins = [ DatalabelsPlugin ];
+//   };
+//   public pieChartData: ChartData<'pie', any[], any | any[]> = {
+//     labels: this.featureService.servName,
+//     // labels: ["Ask","Open"],
 
-  // events
-  public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
-    console.log(event, active);
-  }
+//     datasets:  [ {
+//       data: this.featureService.servSales,
+//       //  data: [100,200]
+//     } ]
+//   };
+  
+//   // pieChartLabels = this.featureService.servName;
+// //   pieChartData:any = [
+// //     { 
+// //         data: this.featureService.servSales
+// //     }
+// // ];
+//   public pieChartType: ChartType = 'pie';
+//   public pieChartPlugins = [ DatalabelsPlugin ];
 
-  public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
-    console.log(event, active);
-  }
+//   // events
+//   public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
+//     console.log(event, active);
+//   }
+
+//   public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
+//     console.log(event, active);
+//   }
 
 
 
@@ -114,4 +182,4 @@ public hi()
 
 // **********************************
 
-}
+
