875699b84581dfebbf39456498ae55b91c19fcea
Waed Sharea <waedshare98@gmail.com>
2022-09-06 00:24:29 +0300

    waed

--- src/app/user/user-about/user-about.component.ts
+++ src/app/user/user-about/user-about.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/Services/login.service';
 import { PostService } from 'src/app/Services/post.service';
 import { UserService } from 'src/app/Services/user.service';
+import * as signalR from "@microsoft/signalr";
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-user-about',
@@ -10,9 +12,30 @@ import { UserService } from 'src/app/Services/user.service';
 })
 export class UserAboutComponent implements OnInit {
 
-  constructor(public userService:UserService,private loginService:LoginService,public post:PostService) { }
-
+  constructor(public userService:UserService,private loginService:LoginService,public post:PostService,private toaster:ToastrService) { }
+  title = 'Frontend';
+  notification:any ;
+ connection = new signalR.HubConnectionBuilder()
+ .configureLogging(signalR.LogLevel.Debug)
+ .withUrl("https://localhost:44324/messageHub", {
+   skipNegotiation: true,
+   transport: signalR.HttpTransportType.WebSockets
+ })
+ .build();
   ngOnInit(): void {
+    this.connection.on("MessageReceived", (message) => {
+      console.log(message);
+      
+      this.notification=message;
+      
+      if(this.notification!=null && this.notification.userToId ==Number(localStorage.getItem('userId')))
+      {
+        
+       this.toaster.success(this.notification.userFrom +" "+this.notification.notificationText);
+
+      }
+    });
+    this.connection.start().catch(err => document.write(err));
     this.loginService.checkIfLoginOrNot();
     this.userService.getUserById(this.loginService.userId);
     this.loginService.getLoginByUserId(this.loginService.userId);
